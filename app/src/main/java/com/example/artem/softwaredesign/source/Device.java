cd26509c3ce8c6e0f69fca2b3fd365352e6f86e4
S1lentem <tema.yarmosh@gmail.com>
2018-10-13 23:46:15 +0300

    Fixed permission request

--- app/src/main/java/com/example/artem/softwaredesign/source/Device.java
+++ app/src/main/java/com/example/artem/softwaredesign/source/Device.java
@@ -1,12 +1,15 @@
 package com.example.artem.softwaredesign.source;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,8 +18,12 @@ import com.example.artem.softwaredesign.R;
 
 
 public class Device extends AppCompatActivity {
+
+    private interface PermissionRequestable{
+        void invoke();
+    }
+
     private final int REQUEST_CODE_PERMISSION_READ_PHONE_STATE = 1;
-    private DevicePhoneData managerDeviceData;
     private TextView imeiView;
 
     @Override
@@ -26,47 +33,53 @@ public class Device extends AppCompatActivity {
 
         TextView versionView = findViewById(R.id.version);
         imeiView = findViewById(R.id.ImeiInfo);
-        managerDeviceData = new DevicePhoneData(this);
+
 
         versionView.setText(String.format(getResources().getString(R.string.version_title), BuildConfig.VERSION_NAME));
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-
-            final Snackbar descriptionSnackbar = Snackbar.make(findViewById(R.id.root),
-                    getResources().getString(R.string.permission_description),
-                    Snackbar.LENGTH_INDEFINITE);
-
-            descriptionSnackbar.setAction(getResources().getString(R.string.ok), new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    descriptionSnackbar.dismiss();
-                }
-            });
-            descriptionSnackbar.show();
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_PHONE_STATE},
-                    REQUEST_CODE_PERMISSION_READ_PHONE_STATE);
+            requestPermissions(Manifest.permission.READ_PHONE_STATE);
         } else {
-            imeiView.setText(String.format(getResources().getString(R.string.imei_title), managerDeviceData.getImei(true)));
+            TelephonyManager manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            imeiView.setText(String.format(getResources().getString(R.string.imei_title), manager.getDeviceId()));
         }
     }
 
+    private void requestPermissions(String ... permissions) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE)) {
 
+            final Snackbar descriptionBar = Snackbar.make(findViewById(R.id.root),
+                    getResources().getString(R.string.permission_description),
+                    Snackbar.LENGTH_INDEFINITE);
+
+            descriptionBar.setAction(getResources().getString(R.string.ok),
+                    view -> ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_PERMISSION_READ_PHONE_STATE))
+                    .show();
+
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_PERMISSION_READ_PHONE_STATE);
+        }
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case REQUEST_CODE_PERMISSION_READ_PHONE_STATE: {
-                imeiView.setText(managerDeviceData.getImei(true));
+                showImei(getResources().getString(R.string.not_access_imei));
             }
         }
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putString("imei", imeiView.getText().toString());
 
-        super.onSaveInstanceState(savedInstanceState);
+    private void showImei(String defaultString) {
+        String imei;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            imei = defaultString;
+        } else {
+            TelephonyManager manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            imei = manager.getDeviceId();
+        }
+        imeiView.setText(String.format(getResources().getString(R.string.imei_title), imei));
     }
 }
